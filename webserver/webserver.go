0a90aa79875f8b77f868916790c09ed4d000fe1c
guoxuivy <guoxuivy@gmail.com>
2016-09-06 15:57:32 +0800

    模版打包进webserver

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -5,10 +5,14 @@ import (
 	"net/http"
 )
 
+const (
+	TMP_DIR = "github.com/guoxuivy/mcron/webserver/template"
+)
+
 //web服务器开始工作（可以用已有的php替换自带的web服务器）
 func WebRun() {
-	http.Handle("/css/", http.FileServer(http.Dir("template")))
-	http.Handle("/js/", http.FileServer(http.Dir("template")))
+	http.Handle("/css/", http.FileServer(http.Dir(TMP_DIR)))
+	http.Handle("/js/", http.FileServer(http.Dir(TMP_DIR)))
 
 	http.HandleFunc("/admin/", adminHandler)
 	http.HandleFunc("/login/", loginHandler)
