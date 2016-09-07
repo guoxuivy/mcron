e8cd50d9e0b31acfd2ce6bfb850354010b8c79cc
guoxuivy <guoxuivy@gmail.com>
2016-09-07 23:35:22 +0800

    添加管理界面，，登陆添加

--- loginController.go
+++ loginController.go
@@ -1,16 +1,19 @@
 package mcron
 
 import (
+	"encoding/json"
 	"html/template"
 	"log"
 	"net/http"
-	"strconv"
 )
 
+type Result struct {
+	Ret    int
+	Reason string
+	Data   interface{}
+}
 type loginController struct {
 }
-
-type List []Job
 
 func (this *loginController) IndexAction(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles(TMP_DIR + "/html/login/index.html")
@@ -20,21 +23,36 @@ func (this *loginController) IndexAction(w http.ResponseWriter, r *http.Request)
 	t.Execute(w, nil)
 }
 
-//添加任务
-func (this *loginController) AddAction(w http.ResponseWriter, r *http.Request) {
-	_id := r.FormValue("id")
-	id, _ := strconv.Atoi(_id)
-	Server.GetSchedule().AddJob(id, "0/5 * * * * ?")
+//登陆提交
+func (this *loginController) LoginAction(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	err := r.ParseForm()
+	if err != nil {
+		OutputJson(w, 0, "参数错误", nil)
+		return
+	}
+
+	admin_name := r.FormValue("admin_name")
+	admin_password := r.FormValue("admin_password")
+
+	if admin_name != "admin" || admin_password != "123456" {
+		OutputJson(w, 0, "账户或密码错误", nil)
+		return
+	}
+
+	// 存入cookie,使用cookie存储
+	cookie := http.Cookie{Name: "admin_name", Value: admin_name, Path: "/"}
+	http.SetCookie(w, &cookie)
+	OutputJson(w, 1, "操作成功", nil)
+	return
 }
 
-//获取当前任务
-func (this *loginController) ListAction(w http.ResponseWriter, r *http.Request) {
-	list := Server.GetSchedule().GetJobs()
-	//log.Println(list)
-	t, err := template.ParseFiles(TMP_DIR + "/html/login/list.html")
+//ajax 返回
+func OutputJson(w http.ResponseWriter, ret int, reason string, i interface{}) {
+	out := &Result{ret, reason, i}
+	b, err := json.Marshal(out)
 	if err != nil {
-		log.Println(err)
+		return
 	}
-	data := map[string]List{"List": list}
-	t.Execute(w, data)
+	w.Write(b)
 }
