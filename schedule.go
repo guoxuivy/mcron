08f24c6ad0421e709753bd6e57d64e79acda92c9
guoxuivy <guoxuivy@gmail.com>
2016-09-12 14:41:27 +0800

    ok

--- schedule.go
+++ schedule.go
@@ -23,19 +23,21 @@ type ScheduleManager struct {
 	//周期任务驱动型任务
 	cronJob *cron.Cron
 	//事件任务驱动型日任务
+	sWorker *scheduleWorker
 }
 
 func NewScheduleManager() *ScheduleManager {
 	instance := &ScheduleManager{}
 	instance.cronJob = cron.New()
 	instance.currentJobs = make(map[int]Job)
+	instance.sWorker = NewscheduleWorker()
 	return instance
 }
 
 func (this *ScheduleManager) Start() {
 	//开启定时任务服务
 	this.cronJob.Start()
-	SheduleWorkerRun()
+	this.sWorker.Start()
 }
 
 func (this *ScheduleManager) Stop() {
@@ -70,7 +72,7 @@ func (this *ScheduleManager) RemoveJob(id int) {
 func (this *ScheduleManager) _scheduleActive(id int) {
 	log.Println("server:任务开始执行-任务ID ******start****", id)
 	job := this.currentJobs[id]
-	go sWorker.sendJob(job)
+	go this.sWorker.sendJob(job)
 }
 
 //节点、配置心跳监听（待实现 或者直接使用zookeeper）
