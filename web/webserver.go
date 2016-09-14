dec84b4725d6243dfa3d06201dfcfc4065b710e3
guoxuivy <guoxuivy@gmail.com>
2016-09-14 16:02:07 +0800

    分类服务端前后台，规划模块

--- web/webserver.go
+++ web/webserver.go
@@ -9,12 +9,11 @@ const (
 	TMP_DIR = "github.com/guoxuivy/mcron/template"
 )
 
-//任务在线处理管道
-var jobChan map[string]chan string
+var jobChan chan string
 
 //web服务器开始工作（可以用已有的php替换自带的web服务器）
-func WebRun(chans map[string]chan string) {
-	jobChan = chans
+func WebRun(c chan string) {
+	jobChan = c
 	http.Handle("/css/", http.FileServer(http.Dir(TMP_DIR)))
 	http.Handle("/js/", http.FileServer(http.Dir(TMP_DIR)))
 
