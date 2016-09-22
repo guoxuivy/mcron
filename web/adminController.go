284275634993d2c20075396c1a59910fcc09b1a6
guoxuivy <guoxuivy@gmail.com>
2016-09-22 15:36:32 +0800

    任务状态添加

--- web/adminController.go
+++ web/adminController.go
@@ -2,7 +2,6 @@ package web
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 )
 
@@ -18,60 +17,22 @@ type Page struct {
 type adminController struct {
 }
 
-//获取joblist
-func (this *adminController) getList() map[int]map[string]string {
-	db, err := getDb()
-	if err != nil {
-		log.Println(err.Error())
-		return nil
-	}
-	//查询数据库
-	query, err := db.Query("SELECT * FROM `job_list` WHERE `status` = ? ", 1)
-	if err != nil {
-		log.Println("查询数据库失败", err.Error())
-		return nil
-	}
-	defer query.Close()
-
-	//读出查询出的列字段名
-	cols, _ := query.Columns()
-	//values是每个列的值，这里获取到byte里
-	values := make([][]byte, len(cols))
-	//query.Scan的参数，因为每次查询出来的列是不定长的，用len(cols)定住当次查询的长度
-	scans := make([]interface{}, len(cols))
-	//让每一行数据都填充到[][]byte里面
-	for i := range values {
-		scans[i] = &values[i]
-	}
-
-	//最后得到的map
-	results := make(map[int]map[string]string)
-	i := 0
-	for query.Next() { //循环，让游标往下推
-		if err := query.Scan(scans...); err != nil { //query.Scan查询出来的不定长值放到scans[i] = &values[i],也就是每行都放在values里
-			log.Println(err)
-			return nil
-		}
-
-		row := make(map[string]string) //每行数据
-
-		for k, v := range values { //每行数据是放在values里面，现在把它挪到row里
-			key := cols[k]
-			row[key] = string(v)
-		}
-		results[i] = row //装入结果集中
-		i++
-	}
-
-	return results
-}
-
 func (this *adminController) IndexAction(w http.ResponseWriter, r *http.Request, user string) {
 	t := AdminTpl("index")
 	model := &jobModel{}
 	list := model.getList()
+	//获取任务运行状态
+	jobChan["job_search"] <- "1"
+	jobstr := <-jobChan["job_list"]
+	ids := []int{}
+	if err := json.Unmarshal([]byte(jobstr), &ids); err == nil {
+		for _, id := range ids {
+			job := list[id]
+			job.Desc = list[id].Desc + " run"
+			list[id] = job
+		}
+	}
 	t.Execute(w, &Page{user, list})
-
 }
 
 //添加任务
@@ -84,14 +45,11 @@ func (this *adminController) AddAction(w http.ResponseWriter, r *http.Request, u
 		scheduleExpr := r.FormValue("scheduleExpr")
 		desc := r.FormValue("desc")
 		//msg := Server.GetSchedule().AddJob(id, scheduleExpr, desc)
-
 		job := &Job{0, scheduleExpr, desc}
-
 		if b, err := json.Marshal(job); err == nil {
 			str := string(b)
 			jobChan["add"] <- str
 		}
-
 		msg := "ok"
 		OutputJson(w, 0, msg, nil)
 	}
